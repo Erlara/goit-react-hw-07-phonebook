b67c93d4de0dd0b7d5aa47a0722398d22677c60a
Larisa Melnichenko <erlara@ukr.net>
2023-04-21 12:17:00 +0300

    add file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,13 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
-import { Filter } from './Filter/Filter';
-import { useDispatch, useSelector } from 'react-redux';
+
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
-import { selectError, selectIsLoading } from 'redux/selectors';
+
 import { useEffect } from 'react';
 
 export function App() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -20,12 +16,12 @@ export function App() {
   return (
     <Layout>
       <GlobalStyle />
-      <h1>Phonebook</h1>
-      <ContactForm />
+      <h1>Tweet cards</h1>
+      {/* <ContactForm />
       {isLoading && !error && <b>Request in progress...</b>}
       <h2>Contacts</h2>
       <Filter />
-      <ContactList />
+      <ContactList /> */}
     </Layout>
   );
 }
--- src/components/Filter/Filter.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Label = styled.label`
-  display: flex;
-  flex-direction: column;
-  row-gap: 5px;
-  margin-top: 15px;
-`;
